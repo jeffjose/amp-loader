ef8014ebdc21e39c0157c957575c74c143500280
Jeffrey Jose <jeffjosejeff@gmail.com>
2019-04-11 19:31:08 -0700

    handle images

--- loaders/amp-loader.js
+++ loaders/amp-loader.js
@@ -1,12 +1,5 @@
 const { JSDOM } = require("jsdom");
 
-const headString = `
-  <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width,minimum-scale=1">
-    <style amp-boilerplate>body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}</style><noscript><style amp-boilerplate>body{-webkit-animation:none;-moz-animation:none;-ms-animation:none;animation:none}</style></noscript>
-    <script async src="https://cdn.ampproject.org/v0.js"></script>
-`;
-
 function handleHTML(node, doc) {
   node.setAttribute("amp", "");
 }
@@ -17,9 +10,6 @@ function handleStyleTag(node, doc) {
   if (styleTags.length == 1) {
     styleTags[0].setAttribute("amp-custom", "");
   } else if (styleTags.length > 1) {
-    console.log("more than 1");
-    console.log(styleTags);
-
     var styleString = "";
     for (var i = 0; i < styleTags.length; i++) {
       tag = styleTags[i];
@@ -35,24 +25,57 @@ function handleStyleTag(node, doc) {
   }
 }
 
+function handleBody(node, doc) {
+  var imgs = doc.querySelectorAll("img");
+
+  for (var i = 0; i < imgs.length; i++) {
+    img = imgs[0];
+
+    ampImg = doc.createElement("amp-img");
+    for (var j = 0; j < img.attributes.length; j++) {
+      attr = img.attributes[j];
+      ampImg.setAttribute(attr.name, attr.value);
+    }
+
+    img.parentNode.insertBefore(ampImg, img);
+    img.remove();
+  }
+}
+
 function handleHead(node, doc) {
   handleStyleTag(node, doc);
 
-  var meta = doc.createElement("meta");
+  var meta = doc.querySelector("meta[charset]");
+  if (meta == undefined) {
+    meta = doc.createElement("meta");
+  }
   meta.setAttribute("charset", "utf-8");
   node.appendChild(meta);
 
-  var meta = doc.createElement("meta");
+  var meta = doc.querySelector("meta[name=viewport]");
+  if (meta == undefined) {
+    meta = doc.createElement("meta");
+  }
   meta.setAttribute("name", "viewport");
   meta.setAttribute("content", "width=device-width,minimum-scale=1");
   node.appendChild(meta);
 
-  var style = doc.createElement("style");
+  var style = doc.querySelector("style[amp-boilerplate]");
+  if (style == undefined) {
+    style = doc.createElement("style");
+  }
   style.setAttribute("amp-boilerplate", "");
   style.textContent = `body{-webkit-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-moz-animation:-amp-start 8s steps(1,end) 0s 1 normal both;-ms-animation:-amp-start 8s steps(1,end) 0s 1 normal both;animation:-amp-start 8s steps(1,end) 0s 1 normal both}@-webkit-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-moz-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-ms-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@-o-keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}@keyframes -amp-start{from{visibility:hidden}to{visibility:visible}}`;
   node.appendChild(style);
 
-  var noscript = doc.createElement("noscript");
+  var noscript = doc.querySelector("noscript");
+  if (noscript == undefined) {
+    noscript = doc.createElement("noscript");
+  } else {
+    // Remove the noscript straight up
+    noscript.remove();
+    noscript = doc.createElement("noscript");
+  }
 
   var style = doc.createElement("style");
   style.setAttribute("amp-boilerplate", "");
@@ -61,18 +84,31 @@ function handleHead(node, doc) {
 
   node.appendChild(noscript);
 
-  var script = doc.createElement("script");
-  script.setAttribute("async", "");
-  script.setAttribute("src", "https://cdn.ampproject.org/v0.js");
-  node.appendChild(script);
+  var scripts = doc.querySelectorAll("script");
+
+  for (var i = 0; i < scripts.length; i++) {
+    script = scripts[i];
+
+    if (!script.src.startsWith("https://cdn.ampproject.org")) {
+      console.log("found random script");
+      // Dont remove because this script doesnt take a stance on whether to remove external javascript
+      //script.remove();
+    } else {
+      script.setAttribute("async", "");
+    }
+  }
+
+  var s = doc.createElement("script");
+  s.setAttribute("async", "");
+  s.setAttribute("src", "https://cdn.ampproject.org/v0.js");
+  node.appendChild(s);
 
   var link = doc.createElement("link");
   link.setAttribute("rel", "canonical");
+  // TODO
   link.setAttribute("href", "foo.html");
   node.appendChild(link);
 }
-
-function handleBody(node, doc) {}
 
 module.exports = function(source) {
   const win = new JSDOM(source).window;
@@ -82,6 +118,7 @@ module.exports = function(source) {
 
   handleHTML(win.document.documentElement, win.document);
   handleHead(head, win.document);
+  handleBody(body, win.document);
 
   source = "<!doctype html> " + win.document.documentElement.outerHTML;
   //console.log(source);
